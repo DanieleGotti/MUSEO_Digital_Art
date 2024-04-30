b8ee2ed3c25e3600382ccf71d018dfc945217c41
DanieleGotti <danielegotti02@gmail.com>
2024-04-30 20:34:23 +0200

    setup codice

---
+++ code/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+    <title>MUSEO START</title>
+    <!--<link rel="stylesheet" href="./css/videoclips.css">-->
+		<!--<script type="text/javascript" src="./js/jquery-2.0.0.js"></script>-->
+		<!--<script type="text/javascript" src="./js/videoclips.js"></script>-->
+	</head>
+
+	<body>
+<?php
+	include 'header.html';
+	include 'nav.html';
+	include 'filtri.html';
+	include 'footer.html'
+?>
+		<div id="content">
+			Tabelle
+
+		</div>
+	</body>
+</html>
