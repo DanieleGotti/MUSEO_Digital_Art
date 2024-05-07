13e8f289142ad93f7f81dc81300c168def3bd763
DanieleGotti <danielegotti02@gmail.com>
2024-05-07 18:20:02 +0200

    Update code

--- code/start.php
+++ code/start.php
@@ -1,20 +1,22 @@
 <!DOCTYPE HTML>
 <html>
-	<head>
-    <title>MUSEO START</title>
-    <!--<link rel="stylesheet" href="./css/videoclips.css">
-		<script type="text/javascript" src="./js/jquery-2.0.0.js"></script>
-		<script type="text/javascript" src="./js/videoclips.js"></script>-->
-	</head>
+<head>
+	<title>MUSEO START</title>
+	<link rel="stylesheet" href="./css/cssStart.css">
+	<script type="text/javascript" src="./js/jsStartScroll.js"></script>
+</head>
 
-	<body>
-		<?php
-			include 'header.html';
-			include 'footer.html'
-		?>
-		<div id="contentStart">
-			<a href="index.php"><button type="button" name="buttonStart">START</button></a>
-		  Testo Intro
+<body>
+	<header class="article-header">
+		<div class="header-content">
+			<h1>MUSEO LOGO</h1>
+			<p>Sottotitolo o nome logo esteso</p>
 		</div>
+		<img class="header-image" src="./img/prova.jpg">
+	</header>
+	<section class="intro">
+		<p>INTRO</p>
+			<a href="./index.php">BOTTONE START</a>
+		</section>
 	</body>
-</html>
+	</html>
