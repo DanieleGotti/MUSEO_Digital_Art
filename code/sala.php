a6cba60cfc4aaaee33b2cc96018762045f33fc77
fgervasoni7 <f.gervasoni7@studenti.unibg.it>
2024-05-14 18:59:18 +0200

    sala

--- code/sala.php
+++ code/sala.php
@@ -13,7 +13,7 @@
 <body>
 
   <?php
-  include 'header.html';
+
   include 'nav.html';
   include 'footer.html'
   ?>
@@ -24,7 +24,14 @@
       <input id="nome" name="nome" type="text" placeholder="Nome"/>
       <input id="superficie" name="superficie" type="text" placeholder="Superficie"/>
       <input id="temaSala" name="temaSala" type="text" placeholder="Tema"/>
+      <input id="descrizione" name="descrizione" type="text" placeholder="Descrizione"/>
       <input type="submit" value="Search"/>
+    </form>
+
+
+    <form name="myform2" method="post">
+    <input id="opera" name="nomeopera" type="text" placeholder="Titolo dell'opera">
+    <input type="submit" value="Cerca per opera">
     </form>
 
     <div id="result">
@@ -35,7 +42,7 @@
       $superficie  = "";
       $temaSala= "";
       $descrizione= "";
-
+      $nomeopera= "";
 
 
       if(count($_POST)>0) {
@@ -44,7 +51,7 @@
         $superficie  = $_POST["superficie"];
         $temaSala  = $_POST["temaSala"];
         $descrizione  = $_POST["descrizione"];
-
+        $nomeopera  = $_POST["nomeopera"];
       }
       else if(count($_GET)>0) {
         $numero = $_GET["numero"];
@@ -52,13 +59,13 @@
         $superficie  = $_GET["superficie"];
         $temaSala  = $_GET["temaSala"];
         $descrizione  = $_GET["descrizione"];
-
+        $nomeopera  = $_GET["nomeopera"];
       }
       include 'salaManager.php';
       $error = false; // Inizializza la variabile $error a false
 
       require_once 'connDb.php';
-      $query = getSalaQry ($numero,	$nome, $superficie ,$temaSala, $descrizione);
+      $query = getSalaQry ($numero,	$nome, $superficie ,$temaSala, $descrizione, $nomeopera);
       echo "<p>numeroQuery: " . $query . "</p>";
 
 
@@ -76,12 +83,12 @@
         <table class="table">
           <tr class="header">
             <!--th>id </th-->
-            <th>idnumero</th>
-            <th>nome</th>
-            <th>superficie</th>
-              <th>temaSala</th>
-              <th>descrizione</th>
-
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Superficie</th>
+            <th>Tema Sala</th>
+            <th>Descrizione</th>
+            <th>Nome Opera</th>
           </tr>
           <?php
           $i=0;
@@ -95,9 +102,9 @@
             $numero = $riga["numero"];
             $nome = $riga["nome"];
             $superficie  = $riga["superficie"];
-              $temaSala = $riga["temaSala"];
-              $descrizione = $riga["descrizione"];
-
+            $temaSala = $riga["temaSala"];
+            $descrizione = $riga["descrizione"];
+            $nomeopera = $riga["nomeopera"];
             ?>
             <tr <?php	echo $classRiga; ?> >
 
@@ -106,6 +113,7 @@
               <td > <?php echo $superficie; ?> </td>
               <td > <?php echo $temaSala; ?> </td>
               <td > <?php echo $descrizione; ?> </td>
+              <td > <?php echo $nomeopera; ?> </td>
 
             </tr>
           <?php } ?>
@@ -115,115 +123,6 @@
     </div>
   </div>
 
-
-    <div id="filtri2">
-      <form name="myform2s" method="POST">
-        <input id="numero" name="numero" type="text" placeholder="Id Sala"/>
-        <input id="nome" name="nome" type="text" placeholder="Nome"/>
-        <input id="superficie" name="superficie" type="text" placeholder="Superficie"/>
-          <input id="temaSala" name="temaSala" type="text" placeholder="Tema"/>
-        <input id="autoripresenti" name="autoripresenti" type="text" placeholder="Autore"/>
-          <input id="operepresenti" name="operepresenti" type="text" placeholder="Titolo Opera"/>
-        <input type="submit" value="Search"/>
-      </form>
-
-      <div id="result2">
-        <?php
-
-        $numero = "";
-        $nome = "";
-        $superficie  = "";
-        $temaSala= "";
-        $descrizione= "";
-        $autoripresenti= "";
-        $operepresenti="";
-
-
-
-        if(count($_POST)>0) {
-          $numero = $_POST["numero"];
-          $nome = $_POST["nome"];
-          $superficie  = $_POST["superficie"];
-          $temaSala  = $_POST["temaSala"];
-          $descrizione  = $_POST["descrizione"];
-          $autoripresenti=$_POST["autoripresenti"];
-          $operepresenti=$_POST["operepresenti"];
-
-        }
-        else if(count($_GET)>0) {
-          $numero = $_GET["numero"];
-          $nome = $_GET["nome"];
-          $superficie  = $_GET["superficie"];
-          $temaSala  = $_GET["temaSala"];
-          $descrizione  = $_GET["descrizione"];
-          $autoripresenti=$_GET["autoripresenti"];
-          $operepresenti=$_GET["operepresenti"];
-
-        }
-        $error = false; // Inizializza la variabile $error a false
-        $query2 = getSalaQry2($numero,	$nome, $superficie ,$temaSala, $descrizione, $autoripresenti, $operepresenti);
-
-
-
-
-        try {
-          $result2 = $conn->query($query2);
-        } catch(PDOException $e) {
-          echo "<p>DB Error on Query: " . $e->getMessage() . "</p>";
-          $error = true;
-        }
-
-        if(!$error) {
-          ?>
-
-          <table class="table">
-            <tr class="header">
-              <!--th>id </th-->
-              <th>idnumero</th>
-              <th>nome</th>
-              <th>superficie</th>
-                <th>temaSala</th>
-                <th>descrizione</th>
-                <th>Nome Autore</th>
-                <th>Cognome Autore</th>
-                <th>Opera</th>
-
-            </tr>
-            <?php
-            $i=0;
-
-            foreach($result2 as $riga2) {
-              $i=$i+1;
-              $classRiga2='class="rowOdd"';
-              if($i%2==0) {
-                $classRiga2='class="rowEven"';
-              }
-              $numero = $riga2["numero"];
-              $nome = $riga2["nome"];
-              $superficie  = $riga2["superficie"];
-                $temaSala = $riga2["temaSala"];
-                $descrizione = $riga2["descrizione"];
-                $autoripresenti = $riga2["autoripresenti"];
-                $operepresentie = $riga2["operepresenti"];
-
-
-              ?>
-              <tr <?php	echo $classRiga; ?> >
-
-                <td > <?php echo $numero; ?> </td>
-                <td > <?php echo $nome; ?> </td>
-                <td > <?php echo $superficie; ?> </td>
-                <td > <?php echo $temaSala; ?> </td>
-                <td > <?php echo $descrizione; ?> </td>
-                <td > <?php echo $autoripresenti; ?> </td>
-                <td > <?php echo $operepresenti; ?> </td>
-              </tr>
-            <?php } ?>
-          </table>
-        <?php }  ?>
-
-      </div>
-    </div>
 
 
 </body>
--- code/salaManager.php
+++ code/salaManager.php
@@ -2,15 +2,14 @@
 
 require_once 'connDb.php';
 
-function getSalaQry($numero, $nome, $superficie, $temaSala, $descrizione): string {
+function getSalaQry($numero, $nome, $superficie, $temaSala, $descrizione, $nomeopera): string {
     global $conn;
 
-    $qry = "SELECT
+    $qry = "SELECT DISTINCT
                 SALA.numero,
                 SALA.nome,
                 SALA.superficie,
-                SALA.temaSala,
-                TEMA.descrizione
+                SALA.temaSala
             FROM
                 SALA
             LEFT JOIN
@@ -33,50 +32,23 @@ function getSalaQry($numero, $nome, $superficie, $temaSala, $descrizione): strin
     if ($descrizione != "")
         $qry .= " AND TEMA.descrizione LIKE '%" . $descrizione . "%'";
 
+    if ($nomeopera != ""){
+        $qry = " SELECT DISTINCT
+                    SALA.numero,
+                    SALA.nome,
+                    SALA.superficie,
+                    SALA.temaSala,
+                    OPERA.titolo AS nomeopera
+                FROM
+                    SALA
+                LEFT JOIN
+                    TEMA ON SALA.temaSala = TEMA.codice
+                JOIN OPERA on OPERA.espostaInSala=SALA.numero
+                WHERE
+                    1=1 AND OPERA.titolo LIKE '%" . $nomeopera . "%'";
+}
     return $qry;
 }
 
-//seconda query unisco autore ed opere
-function getSalaQry2($numero, $nome, $superficie, $temaSala, $descrizione, $autoripresenti, $operepresenti): string {
-    global $conn;
-
-    $query2 = "SELECT
-                SALA.numero,
-                SALA.nome,
-                SALA.superficie,
-                SALA.temaSala,
-                AUTORE.cognome,
-                OPERA.titolo
-            FROM
-                SALA
-            JOIN
-                OPERA ON SALA.numero = OPERA.espostaInSala
-            JOIN
-                AUTORE ON OPERA.autore = AUTORE.codice
-            
-
-            WHERE
-                1=1";
-
-    if ($numero != "")
-        $query2 .= " AND SALA.numero = '$numero'";
-
-    if ($nome != "")
-        $query2 .= " AND SALA.nome LIKE '%" . $nome . "%'";
-
-    if ($superficie != "")
-        $query2 .= " AND SALA.superficie LIKE '%" . $superficie . "%'";
-
-    if ($temaSala != "")
-        $query2 .= " AND SALA.temaSala LIKE '%" . $temaSala . "%'";
-
-    if ($autoripresenti != "")
-        $query2 .= " AND AUTORE.cognome LIKE '%" . $autoripresenti . "%' ";
-
-    if ($operepresenti != "")
-        $query2 .= " AND OPERA.titolo LIKE '%" . $operepresenti . "%'";
-
-    return $query2;
-}
 
 ?>
