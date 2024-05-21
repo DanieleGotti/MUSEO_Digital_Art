56342bd3dd555e68e22f47602853bfc3ec03c315
fgervasoni7 <f.gervasoni7@studenti.unibg.it>
2024-05-21 18:30:45 +0200

    Update tema.php

--- code/tema.php
+++ code/tema.php
@@ -1,106 +1,148 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-  <title>temi</title>
-  <link rel="stylesheet" href="./css/cssTema.css">
+  <title>Temi</title>
+  <link rel="stylesheet" href="./css/cssPage.css">
+
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap">
   <script type="text/javascript" src="./js/jquery-2.0.0.js"></script>
+  <script type="text/javascript" src="./js/jsFiltersMove.js"></script>
 </head>
 
 <body>
+<?php
+  $page = 'Temi';
+  include 'header.php';
 
-  <?php
-  include 'header.html';
+
   include 'nav.html';
-  ?>
+?>
 
-  <div class="back">
-    <div id="filtri" class="filters">
-      <form name="myform" method="POST">
-        <ul class="filterContainer">
+  <button id="bottoneFiltri" class="filterButton" onclick="moveFilters()"  onmouseenter="animateIcon(this)" onmouseleave="animateIcon(this)">
+    <img src="../img/filtroStatica.png">
+  </button>
+
+  <div id="filtri" class="filters">
+    <form name="myform" class="form" method="POST">
+      <ul class="filterContainer">
+        <li class="filterHeader">
+          <span class="filterTitle">Filtri</span>
+        </li>
+        <li class="filterItem">
+          <button type="submit" class="button">
+            <span class="buttonText">Cerca</span>
+            <span class="buttonIcon">
+              <img src="../img/cerca.png">
+            </span>
+          </button>
+          <button type="submit" class="button">
+            <span class="buttonText">Reset</span>
+            <span class="buttonIcon">
+              <img src="../img/reset.png">
+            </span>
+          </button>
+        </li>
+
+        <div class="filterScroll">
           <li class="filterItem">
-            <input id="codice" class="input" name="codice" type="text" placeholder=""/>
+            <input id="codice" class="input" name="codice" type="text"/>
+
             <label class="placeHolder">Codice</label>
           </li>
           <li class="filterItem">
-            <input id="descrizione" class="input" name="descrizione" type="text" placeholder=""/>
+            <input id="descrizione" class="input" name="descrizione" type="text"/>
             <label class="placeHolder">Descrizione</label>
           </li>
-          <li class="filterItem">
-            <input type="submit" value="Search"/>
-          </li>
-          <li class="filterItem">
-            <input type="submit" value="RESET"/>
-          </li>
-        </ul>
-      </form>
-    </div>
+        </div>
+      </ul>
+    </form>
+  </div>
 
-    <div id="result" class="main">
-      <?php
 
-      $codice = "";
-      $descrizione = "";
-      $numeroSale  = "";
+  <div id="result" class="tableBody">
+<?php
+  $codice = "";
+  $descrizione = "";
+  $numeroSale  = "";
 
-      if(count($_POST)>0) {
-        $codice = $_POST["codice"];
-        $descrizione = $_POST["descrizione"];
-        $numeroSale  = $_POST["numeroSale"];
-      }
-      else if(count($_GET)>0) {
-        $codice = $_GET["codice"];
-        $descrizione = $_GET["descrizione"];
-        $numeroSale  = $_GET["numeroSale"];
-      }
-      include 'temaManager.php';
-      $error = false;
+  if(count($_POST)>0) {
+    $codice = $_POST["codice"];
+    $descrizione = $_POST["descrizione"];
+    $numeroSale  = $_POST["numeroSale"];
+  }
+  else if(count($_GET)>0) {
+    $codice = $_GET["codice"];
+    $descrizione = $_GET["descrizione"];
+    $numeroSale  = $_GET["numeroSale"];
+  }
+  include 'temaManager.php';
+  $error = false;
 
-      require_once 'connDb.php';
-      $query = getTemaQry ($codice,	$descrizione, $numeroSale );
+  require_once 'connDb.php';
+  $query = getTemaQry ($codice,	$descrizione, $numeroSale );
 
-      try {
-        $result = $conn->query($query);
-      } catch(PDOException $e) {
-        echo "<p>DB Error on Query: " . $e->getMessage() . "</p>";
-        $error = true;
-      }
+  try {
+    $result = $conn->query($query);
+  } catch(PDOException $e) {
+    echo "<p>DB Error on Query: " . $e->getMessage() . "</p>";
+    $error = true;
+  }
+  if(!$error) {
+?>
+    <table>
+        <thead>
+          <tr>
+            <th>CODICE TEMA
+              <button class="iconArrow">
+                <img src="./img/freccia.png">
+              </button>
+            </th>
+            <th>DESCRIZIONE
+              <button class="iconArrow">
+                <img src="./img/freccia.png">
+              </span>
+            </th>
+            <th>NUMERO SALE
+              <button class="iconArrow">
+                
+                <img src="./img/freccia.png">
+              </button>
+            </th>
 
-      if(!$error) {
-        ?>
-
-        <table class="table">
-          <tr class="tableHeader">
-            <!--th>id </th-->
-            <th>idcodice</th>
-            <th>descrizione</th>
-            <th>numeroSale</th>
           </tr>
+        </thead>
+        <tbody>
+<?php
+  $i=0;
+  foreach($result as $riga) {
+    $i=$i+1;
+    $classRiga='class="rowOdd"';
+    if($i%2==0) {
+    $classRiga='class="rowEven"';
+  }
+  $codice = $riga["codice"];
+  $descrizione = $riga["descrizione"];
+  $numeroSale  = $riga["numeroSale"];
+?>
 
-          <?php
-          $i=0;
 
-          foreach($result as $riga) {
-            $i=$i+1;
-            $classRiga='class="rowOdd"';
-            if($i%2==0) {
-              $classRiga='class="rowEven"';
-            }
-            $codice = $riga["codice"];
-            $descrizione = $riga["descrizione"];
-            $numeroSale  = $riga["numeroSale"];
-            ?>
 
             <tr <?php	echo $classRiga; ?> >
               <td > <?php echo $codice; ?> </td>
               <td > <?php echo $descrizione; ?> </td>
-              <td > <?php echo $numeroSale; ?> </td>
-            </tr>
-          <?php } ?>
-        </table>
 
-      <?php }  ?>
-    </div>
+              <td>
+    <a href="sala.php?temaSala=<?php echo urlencode($codice); ?>">
+      <?php echo $numeroSale; ?>
+    </a>
+  </td>
+
+            </tr>
+<?php } ?>
+        </tbody>
+      </table>
+<?php }  ?>
+
   </div>
 
 
